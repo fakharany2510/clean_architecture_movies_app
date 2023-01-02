e13ecd73ab0ae874e3782f19c09db04fc4e812bb
fakharany2510 <fakharany00@gmail.com>
2023-01-02 14:06:54 +0200

    some design modifications

--- lib/start_Screen.dart
+++ lib/start_Screen.dart
@@ -20,7 +20,7 @@ class StartScreen extends StatelessWidget {
                 Navigator.push(context, MaterialPageRoute(builder: (context)=>const MainMoviesScreen()));
               },
               child: Container(
-                width: 150,
+                width: 159,
                 height: 50,
                 decoration: BoxDecoration(
                   color: Theme.of(context).bottomAppBarColor,
